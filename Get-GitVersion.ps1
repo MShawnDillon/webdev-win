5dd84e3adf2602249f2f09fb2435d1612691e413
M. Shawn Dillon <generichuman4897@gmail.com>
2014-07-02 21:47:40 -0400

    Add Get-GitVersion.ps1

---
+++ Get-GitVersion.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    Gets the Git version that is available in the current environment.
+.DESCRIPTION
+    This calls the git --version command if the command is available and
+    returns a System.Version representing the currently available git version.
+.EXAMPLE
+    Get-GitVersion
+.OUTPUT
+    System.Nullable[System.Version] Either the version returned from Git (with
+    the msysgit specification removed), or $null if git is not available.
+.NOTES
+    Original Author: M. Shawn Dillon
+#Requires -Version 2.0
+#>
+function Get-GitVersion
+{
+    try
+    {
+        $command = Get-Command git.exe -ErrorAction SilentlyContinue
+
+        $gitVersion = git.exe `-`-version
+
+        $match = [Regex]::Match( `
+            $gitVersion, `
+            '^git version (?<Version>\d+\.\d+\.\d+)\.msysgit\.(?<Revision>\d+)$')
+
+        if ($match.Success -eq $false)
+        {
+            return $null
+        }
+
+        $version = [Version]($match.Groups[1].Value + '.' + $match.Groups[2].Value)
+
+        return $version
+    }
+    catch [System.Management.Automation.CommandNotFoundException]
+    {
+        return $null
+    }
+}
