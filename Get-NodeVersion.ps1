e50bdfe184f50c7dd5f7993c026a13618e99172f
M. Shawn Dillon <generichuman4897@gmail.com>
2014-07-02 21:55:38 -0400

    Add Get-NodeVersion.ps1

---
+++ Get-NodeVersion.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    Gets the Node version that is available in the current environment.
+.DESCRIPTION
+    This calls the node --version command if the command is available and
+    returns a System.Version representing the currently available Node
+    version.
+.EXAMPLE
+    Get-NodeVersion
+.OUTPUT
+    System.Nullable[System.Version] Either the version returned from Node,
+    or $null if Node is not available.
+.NOTES
+    Original Author: M. Shawn Dillon
+#Requires -Version 2.0
+#>
+function Get-NodeVersion
+{
+    try
+    {
+        $command = Get-Command node.exe -ErrorAction SilentlyContinue
+
+        $nodeVersion = node.exe `-`-version
+
+        $match = [Regex]::Match( `
+            $nodeVersion, `
+            '^v(?<Version>\d+\.\d+\.\d+)$')
+
+        if ($match.Success -eq $false)
+        {
+            return $null
+        }
+
+        $version = [Version]($match.Groups[1].Value)
+
+        return $version
+    }
+    catch [System.Management.Automation.CommandNotFoundException]
+    {
+        return $null
+    }
+}
